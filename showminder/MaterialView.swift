53422fd2a90afeff2ff6bcde26052f7dc56ca481
Jareedos <jaredallens@gmail.com>
2016-09-06 15:34:00 -0700

    Changed launchScreen View & added MaterialVew file for custom shadowing

---
+++ showminder/MaterialView.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialView.swift
+//  showminder
+//
+//  Created by Jared Sobol on 9/6/16.
+//  Copyright © 2016 Appmaker. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
